836f22cd06f14bf8895cc87771aece06f1731766
yuxiaozhou <yuxiaozhou@>
2017-04-05 10:29:48 +0800

    feature:Init Project

---
+++ RabbitMQTest/Program.cs
@@ -0,0 +1,129 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQTest
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RabbitMqFactory rmf = null;
+            try
+            {
+                 rmf = RabbitMqFactory.Get();
+
+                rmf.Init("192.168.174.128", "user", "123456");
+                rmf.GetChannel("消息队列1", (channel) =>
+                {
+                    channel.ExchangeDeclare(exchange: "hello-exchange", type: ExchangeType.Direct, durable: true, autoDelete: false);
+                    channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false);
+                    channel.QueueBind(queue: "hello-queue", exchange: "hello-exchange", routingKey: "hello");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    var consumerTag = channel.BasicConsume("hello-queue", false, consumer);
+
+                    consumer.Received += (model, e) =>
+                    {
+                        channel.BasicAck(e.DeliveryTag, false);
+                        var message = Encoding.UTF8.GetString(e.Body);
+                        Console.WriteLine("Received {0}", message);
+                    };
+                });
+
+            }
+            catch (Exception ex)
+            {
+                if (rmf != null)
+                {
+                    rmf.Dispose();
+                }
+                //记录日志
+
+                Console.WriteLine(ex.Message + " stack" + ex.StackTrace);
+            }
+
+            Console.ReadLine();
+
+           
+
+            //var factory = new ConnectionFactory() { HostName = "192.168.174.128", UserName = "user", Password = "123456" };
+
+            //常规写法1
+            //while (true)
+            //{
+            //    //获取一个连接
+
+            //    using (var connection = factory.CreateConnection())
+            //    {
+            //        //如果连接没被意外关闭
+            //        while (connection.IsOpen)
+            //        {
+            //            //获取渠道中未处理消息
+            //            using (var channel = connection.CreateModel())
+            //            {
+            //                channel.ExchangeDeclare(exchange: "hello-exchange", type: ExchangeType.Direct, durable: true, autoDelete: false);
+            //                channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false);
+            //                channel.QueueBind(queue: "hello-queue", exchange: "hello-exchange", routingKey: "hello");
+
+            //                //第二个参数是自动应答
+            //                QueueingBasicConsumer consumer = new QueueingBasicConsumer(channel);
+
+
+            //                channel.BasicConsume("hello-queue", false, consumer);
+
+            //                while (true)
+            //                {
+            //                    var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();//接收消息并出列
+            //                    var body = ea.Body;//消息主体
+            //                    var message = Encoding.UTF8.GetString(body);
+            //                    Console.WriteLine("Received {0}", message);
+            //                    if (message == "exit")
+            //                    {
+            //                        // 处理应答
+            //                        Console.WriteLine("exit!");
+            //                        break;
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            
+            //常规写法2
+
+            //var factory = new ConnectionFactory() { HostName = "192.168.174.128", UserName = "user", Password = "123456" };
+            //using (var connection = factory.CreateConnection())
+            //{
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        channel.ExchangeDeclare(exchange: "hello-exchange", type: ExchangeType.Direct, durable: true, autoDelete: false);
+            //        channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false);
+            //        channel.QueueBind(queue: "hello-queue", exchange: "hello-exchange", routingKey: "hello");
+
+            //        var consumer = new EventingBasicConsumer(channel);
+            //        var consumerTag = channel.BasicConsume("hello-queue", false, consumer);
+
+            //        consumer.Received += (model, e) =>
+            //        {
+            //            channel.BasicAck(e.DeliveryTag, false);
+            //            var message = Encoding.UTF8.GetString(e.Body);
+            //            Console.WriteLine("Received {0}", message);
+            //        };
+
+            //        if ("q" == Console.ReadLine())
+            //        {
+            //            channel.BasicCancel(consumerTag);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
---
+++ RabbitMQTest/RabbitMqFactory.cs
@@ -0,0 +1,180 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQTest
+{
+    public class RabbitMqFactory : IDisposable
+    {
+        private static RabbitMqFactory _simple;
+        private static object _objS = new object();
+
+        private IConnectionFactory _factory;
+        private IConnection _connection;
+        private object _obj;
+        private Dictionary<string, IModel> _modelDict;
+
+        public static RabbitMqFactory Get()
+        {
+            if (_simple == null)
+            {
+                lock (_objS)
+                {
+                    if (_simple == null)
+                    {
+                        _simple = new RabbitMqFactory();
+                    }
+                }
+            }
+
+            return _simple;
+        }
+
+        private RabbitMqFactory()
+        {
+            _obj = new object();
+            _modelDict = new Dictionary<string, IModel>();
+        }
+
+        /// <summary>
+        /// 初始化工厂
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        public void Init(string hostname, string username, string password)
+        {
+            _factory = new ConnectionFactory() { HostName = hostname, UserName = username, Password = password };
+        }
+
+        /// <summary>
+        /// 获取一个连接 如果不存在或者关闭 则重新开启一个
+        /// </summary>
+        /// <returns></returns>
+        public IConnection GetConnection()
+        {
+            if (_factory == null)
+            {
+                throw new Exception("请先初始化");
+            }
+
+            if (_connection == null || !_connection.IsOpen)
+            {
+                lock (_obj)
+                {
+                    if (_connection != null)
+                    {
+                        _connection.Close();
+                        _connection.Dispose();
+                    }
+                    _connection = _factory.CreateConnection();
+                }
+            }
+
+            return _connection;
+        }
+
+
+        /// <summary>
+        /// 获取(如果不存在新建)一个渠道
+        /// </summary>
+        /// <param name="channelName"></param>
+        /// <param name="channeclwork"></param>
+        /// <returns></returns>
+        public IModel GetChannel(string channelName, Action<IModel> channeclwork)
+        {
+            IModel result = null;
+            if (!_modelDict.ContainsKey(channelName) || _modelDict[channelName].IsOpen)
+            {
+                //因为如果连接失效并重连 因停止获取Channel的定义 所以用一个锁
+                lock (_obj)
+                {
+                    if (_modelDict.ContainsKey(channelName))
+                    {
+                        _modelDict[channelName].Close();
+                        _modelDict[channelName].Dispose();
+                        _modelDict.Remove(channelName);
+                    }
+                    result = GetConnection().CreateModel();
+                    channeclwork(result);
+                    _modelDict.Add(channelName, result);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 释放
+        /// </summary>
+        public void Dispose()
+        {
+            try
+            {
+                DisposeModel();
+
+                DisposeConnection();
+
+                DisposeFactory();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("尝试关闭RMQ队列失败!" + ex.Message + " stack" + ex.StackTrace);
+            }
+
+            
+        }
+
+        /// <summary>
+        /// 释放工厂
+        /// </summary>
+        private void DisposeFactory()
+        {
+            if (_factory != null)
+            {
+                _factory = null;
+            }
+        }
+
+        /// <summary>
+        /// 释放连接
+        /// </summary>
+        private void DisposeConnection()
+        {
+            if (_connection != null)
+            {
+                if (_connection.IsOpen)
+                {
+                    _connection.Close();
+                }
+                _connection.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 释放model
+        /// </summary>
+        private void DisposeModel()
+        {
+            if (_modelDict != null && _modelDict.Count > 0)
+            {
+                foreach (var item in _modelDict)
+                {
+                    if (item.Value != null)
+                    {
+                        if (item.Value.IsOpen)
+                        {
+                            item.Value.Close();
+                        }
+                        item.Value.Dispose();
+                    }
+                }
+                _modelDict = null;
+            }
+        }
+    }
+}
